6f33fc4829eaef62e0bcfeaf3e14ddb2b429ca11
AP Playground <143913641+AP-Playground@users.noreply.github.com>
2025-08-02 00:39:24 -0700

    init

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/react-gh-pages' : '',
+  assetPrefix: isProd ? '/react-gh-pages/' : ''
+};
+
+
+export default nextConfig;
